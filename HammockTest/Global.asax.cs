44275fc61042c8e92a068e58331c9c184cb57bd8
Michael Friis <friism@gmail.com>
2012-04-17 15:25:19 -0700

    clean up MvcApplication

--- HammockTest/Global.asax.cs
+++ HammockTest/Global.asax.cs
@@ -4,9 +4,6 @@ using System.Web.Routing;
 
 namespace HammockTest
 {
-	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-	// visit http://go.microsoft.com/?LinkId=9394801
-
 	public class MvcApplication : System.Web.HttpApplication
 	{
 		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
@@ -23,12 +20,10 @@ namespace HammockTest
 				"{controller}/{action}/{id}", // URL with parameters
 				new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
 			);
-
 		}
 
 		protected void Application_Start()
 		{
-			//ServicePointManager.CertificatePolicy = new NumskullCertificatePolicy();
 			AreaRegistration.RegisterAllAreas();
 
 			RegisterGlobalFilters(GlobalFilters.Filters);
