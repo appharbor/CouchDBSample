8deb48b14e3a33b6fd88d67ecbb23925acf6ccd4
Michael Friis <friism@gmail.com>
2012-04-17 15:24:58 -0700

    re-order usings

--- HammockTest/Global.asax.cs
+++ HammockTest/Global.asax.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Net;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Net;
 
 namespace HammockTest
 {
